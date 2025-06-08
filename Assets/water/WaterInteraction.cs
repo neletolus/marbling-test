e45d75d35ae407abb47be60202805930003c7eb5
Yohei Suda <y1405705@gmail.com>
2025-06-08 17:50:03 +0900

    remove unused paintbin assets and add water interaction script

--- Assets/water/WaterInteraction.cs
+++ Assets/water/WaterInteraction.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class redbinrotate : MonoBehaviour
+public class WaterInteraction : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
